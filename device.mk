af936b45128c76c5c5856f3267c2391ea0aa7914
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    fuxi: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- device.mk
+++ device.mk
@@ -383,6 +383,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     CarrierConfigRes \
     CarrierConfigResMiui \
+    DeviceAsWebcamResXiaomi \
     FrameworksResCommon \
     FrameworksResFuxi \
     SettingsResCommon \
