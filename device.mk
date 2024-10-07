a29b34988b3ea21434b2afb65ac3edc3c91aee50
ralf979 <ralf979@gmail.com>
2024-10-07 15:48:22 +0200

    sm8550-common: update sensors to v2

--- device.mk
+++ device.mk
@@ -477,7 +477,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
     libsensorndkbridge \
     sensor-notifier \
-    sensors.xiaomi
+    sensors.xiaomi.v2
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_kalama/android.hardware.sensor.accelerometer.xml \
