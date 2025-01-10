2d5b35f8bc7c27ea98b40891b11f414cb246ec5e
Quang Minh <38067026+lonelystar9x@users.noreply.github.com>
2025-01-10 07:33:20 +0700

    Revert "fuxi: Add hotword enrollment blobs"
    This reverts commit c2e245df3b3cc33d0f827e6d10606f5175c96be2.

--- device.mk
+++ device.mk
@@ -270,10 +270,6 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
-# Hotword Enrollement
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
 # Identity
 PRODUCT_PACKAGES += \
     android.hardware.identity-V4-ndk.vendor
